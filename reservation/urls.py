bef43fedc0a1cbd445aab49ce6ebbb38a2d4b110
TakumiIsibasi <fko2347037@stu.o-hara.ac.jp>
2024-11-01 10:00:45 +0900

    利用者の画面url追加

--- reservation/urls.py
+++ reservation/urls.py
@@ -8,14 +8,28 @@ urlpatterns = [
     path("userlogin", views.userlogin, name="userlogin"),
     # ログイン後ホーム画面
     path("mainmenu", views.mainmenu, name="mainmenu"),
+    # ログアウト画面
+    path("logout", views.logout, name="logout"),
     # 新規登録画面
     path("usersignup", views.usersignup, name="usersignup"),
+    # 新規登録完了画面
+    path("signupcompleted", views.signupcompleted, name="signupcompleted"),
     # 予約画面
     path("cleaningappointment", views.cleaningappointment, name="cleaningappointment"),
     # 予約完了画面
     path("reservationcompleted", views.reservationcompleted, name="reservationcompleted"),
-    # 予約詳細画面
-    path("userreservationdetails", views.userreservationdetails, name="userreservationdetails"),
+    #予約変更画面
+    path("reservationchange", views.reservationchange, name="reservationchange"),
+    #予約変更完了画面
+    path("reservationchangecompleted", views.reservationchangecompleted, name="reservationchangecompleted"),
+    #予約キャンセル完了画面
+    path("reservationcancellationcompleted", views.reservationcancellationcompleted, name="reservationcancellationcompleted"),
     # 予約一覧画面
     path("userreservationlistscreen", views.userreservationlistscreen, name="userreservationlistscreen"),
+    # 予約詳細画面
+    path("userreservationdetails", views.userreservationdetails, name="userreservationdetails"),
+    # お問い合わせ画面
+    path("contactus", views.contactus, name="contactus"),
+    # お問い合わせ送信完了画面
+    path("submissioncomplete", views.submissioncomplete, name="submissioncomplete"),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -12,9 +12,17 @@ def userlogin(request):
 def mainmenu(request):
     return render(request, 'mainmenu.html')
 
+# ログアウト画面
+def logout(request):
+    return render(request, 'logout.html')
+
 # 新規登録画面
 def usersignup(request):
     return render(request, 'usersignup.html')
+
+# 新規登録完了画面
+def signupcompleted(request):
+    return render(request, 'signupcompleted.html')
 
 # 予約画面
 def cleaningappointment(request):
@@ -24,10 +32,32 @@ def cleaningappointment(request):
 def reservationcompleted(request):
     return render(request, 'reservationcompleted.html')
 
-# 予約詳細画面
-def userreservationdetails(request):
-    return render(request, 'userreservationdetails.html')
+# 予約変更画面
+def reservationchange(request):
+    return render(request, 'reservationchange.html')
+
+# 予約変更完了画面
+def reservationchangecompleted(request):
+    return render(request, 'reservationchangecompleted.html')
+
+# 予約キャンセル完了画面
+def reservationcancellationcompleted(request):
+    return render(request, 'reservationcancellationcompleted.html')
 
 # 予約一覧画面
 def userreservationlistscreen(request):
     return render(request, 'userreservationlistscreen.html')
+
+# 予約詳細画面
+def userreservationdetails(request):
+    return render(request, 'userreservationdetails.html')
+
+# お問い合わせ画面
+def contactus(request):
+    return render(request, 'contactus.html')
+
+# お問い合わせ送信完了画面
+def submissioncomplete(request):
+    return render(request, 'submissioncomplete.html')
+
+
