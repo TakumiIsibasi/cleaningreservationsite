0dff1126320eb698df2623f9a07c3ed3b23db073
KantaNakashima <fko2347007@stu.o-hara.ac.jp>
2024-11-07 14:16:55 +0900

    幹太　変更11

--- reservation/urls.py
+++ reservation/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name = "reservation" 
 
 urlpatterns = [
-    path("reservation:mainmenu", views.mainmenu, name="mainmenu"), 
-    path("reservation:cleaningappointment", views.cleaningappointment, name="cleaningappointment"),
-    path("reservation:reservationcompleted", views.reservationcompleted, name="reservationcompleted"),
-    path("reservation:reservationchange", views.reservationchange, name="reservationchange"),
-    path("reservation:reservationchangecompleted", views.reservationchangecompleted, name="reservationchangecompleted"),
-    path("reservation:reservationcancellationcompleted", views.reservationcancellationcompleted, name="reservationcancellationcompleted"),
-    path("reservation:userreservationlistscreen", views.userreservationlistscreen, name="userreservationlistscreen"),
-    path("reservation:userreservationdetails", views.userreservationdetails, name="userreservationdetails"),  
+    path("mainmenu", views.mainmenu, name="mainmenu"), 
+    path("cleaningappointment", views.cleaningappointment, name="cleaningappointment"),
+    path("reservationcompleted", views.reservationcompleted, name="reservationcompleted"),
+    path("reservationchange", views.reservationchange, name="reservationchange"),
+    path("reservationchangecompleted", views.reservationchangecompleted, name="reservationchangecompleted"),
+    path("reservationcancellationcompleted", views.reservationcancellationcompleted, name="reservationcancellationcompleted"),
+    path("userreservationlistscreen", views.userreservationlistscreen, name="userreservationlistscreen"),
+    path("userreservationdetails", views.userreservationdetails, name="userreservationdetails"),  
 ]
