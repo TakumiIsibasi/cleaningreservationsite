ba172728ef27902febe738f8cf4616646a67f24a
TakumiIsibasi <fko2347037@stu.o-hara.ac.jp>
2025-01-16 11:22:09 +0900

    みもみもみもみも

--- reservation/forms.py
+++ reservation/forms.py
@@ -1,20 +1,31 @@
 from django import forms
 from .models import UserReservation
- 
-# 予約用フォーム
-# モデル`user_reservation`に基づくフォームを作成
+from datetime import datetime, timedelta
+
 class UserForm(forms.ModelForm):
     class Meta:
-        # 使用するモデルを指定
         model = UserReservation
-       
-        # フォームに含めるフィールドを指定
         fields = [
-            "user_name",             # ユーザーの名前
-            "user_phone",            # ユーザーの電話番号
-            "user_email",            # ユーザーのメールアドレス
-            "user_address",           # ユーザーの住所
-            "user_cleaning_location",# 清掃の実施場所
-            "user_cleaning_date",    # 清掃の希望日時
-            "user_comments"          # 備考欄
+            "user_name",
+            "user_phone",
+            "user_email",
+            "user_address",
+            "user_cleaning_location",
+            "user_cleaning_date",
+            "user_comments"
         ]
+        widgets = {
+            "user_cleaning_date": forms.DateTimeInput(
+                attrs={
+                    "type": "datetime-local",
+                    "class": "form-control",  # Bootstrap などのクラスを適用
+                }
+            )
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 現在日から3日後を計算
+        min_date = (datetime.now() + timedelta(days=3)).strftime("%Y-%m-%dT%H:%M")
+        # user_cleaning_date フィールドに min 属性を追加
+        self.fields['user_cleaning_date'].widget.attrs['min'] = min_date
