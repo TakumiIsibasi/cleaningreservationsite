e5cdc26b2ebd4f607fe22890e2108676c0085cd2
riichi <fko2347076@stu.o-hara.ac.jp>
2025-02-07 12:05:11 +0900

    舞台少女は次の舞台へ！！

--- adminaccounts/forms.py
+++ adminaccounts/forms.py
@@ -1,7 +1,9 @@
 # forms.py
 from django import forms
 from django.contrib.auth import get_user_model  # カスタムユーザーモデルをインポート
+from accounts.models import User
 
+#従業員変更
 class EmployeeUpdateForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
@@ -16,5 +18,17 @@ class EmployeeUpdateForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'is_active': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'phone', 'role', 'is_active']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'role': forms.Select(attrs={'class': 'form-select'}),
             'is_active': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
