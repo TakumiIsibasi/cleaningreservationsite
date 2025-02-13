9bbde7e561a451c2a0b3d6a7ef828ee7bafff3ba
TakumiIsibasi <fko2347037@stu.o-hara.ac.jp>
2025-02-13 09:46:40 +0900

    すべてを終わらせる

--- adminaccounts/urls.py
+++ adminaccounts/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
 from . import views
-
-app_name = "adminaccounts" 
-
+ 
+app_name = "adminaccounts"
+ 
 urlpatterns = [
     path("", views.adminhome, name="adminhome"),
     path("adminemployeelist", views.adminemployeelist, name="adminemployeelist"),
     path("adminrequestconfirmation", views.adminrequestconfirmation, name="adminrequestconfirmation"),
     path("adminemployeeschedulelist", views.adminemployeeschedulelist, name="adminemployeeschedulelist"),
     path("administratorrequestschedulechange", views.administratorrequestschedulechange, name="administratorrequestschedulechange"),
-    path("administratorrequestscheduleconfirmation", views.administratorrequestscheduleconfirmation, name="administratorrequestscheduleconfirmation"), 
-    path("employee_update/<uuid:employee_id>/", views.employee_update, name="employee_update"), 
+    path("administratorrequestscheduleconfirmation", views.administratorrequestscheduleconfirmation, name="administratorrequestscheduleconfirmation"),
+    path("employee_update/<uuid:employee_id>/", views.employee_update, name="employee_update"),
     path("adminrequestconfirmation/", views.adminrequestconfirmation, name='adminrequestconfirmation'),
     path("adminemployeeschedulelist/", views.adminemployeeschedulelist, name='adminemployeeschedulelist'),
-    path('employee/add/', views.add_employee, name='employee_add'),
 ]
